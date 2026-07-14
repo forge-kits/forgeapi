656ae33870682b42a31e6949086e9dab4048baf3
VsevolodKrasovskyi <sevakrasovskiy@gmail.com>
2026-07-14 18:34:14 +0300

    fix(lint): remove unused asyncio and Any imports

--- forgeapi/database/queryset.py
+++ forgeapi/database/queryset.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from tortoise.manager import Manager
 from tortoise.queryset import QuerySet
